deb013faa675d8d0e6f842157fed588010d368db
454906075@qq.com <454906075@qq.com>
2016-08-16 14:33:40 +0800

    Basic Routing

--- myapp.js
+++ myapp.js
@@ -2,11 +2,41 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res){
-	res.send('Hello world!');
+//	res.send('Hello world!');
+	res.json({status:'my api is alive!'});
 });
 
 
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!')
-})
+});
+
+app.post('/', function (req,res){
+	res.send('Got a post request');
+});
+
+app.put('/user',function(req,res){
+	res.send('Got a put request at /user');
+});
+
+app.delete('/user',function(req, res){
+	res.send('Got a DELETE request at /userOB');
+});
+
+
+//app.use(express.static('public'));
+
+//app.use('/static', express.static('public');
+app.use('/res', express.static('public'));
+
+app.get('/v1/test-api', function(req,res){
+	res.json({ret:1000});
+});
+
+app.get('/v2/:id', function(req,res){
+	var test = req.params.id;
+	res.json({test: test});
+});
+
+
 
