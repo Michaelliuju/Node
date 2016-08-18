8c49b4a46bd29e6d79a854267f7f6edf9235572a
454906075@qq.com <454906075@qq.com>
2016-08-18 11:56:09 +0800

    重新修改 serving a pug

--- serving_a_pug.js
+++ serving_a_pug.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var app = express();
-var router = express.Router();
 
-app.listen(3000,function(){
+var server = app.listen(3000,function(){
+	var host = server.address().address;
+	var port = server.address().port;
 	console.log('its starting');
-});
+	console.log('Example app listening at http://%s:%s', host, port);
+
+})
 
 app.set('views', './public')
 
 app.set('view engine', 'pug');
 
+var router = express.Router();
+
 router.get('/public/:id', function (req, res) {
-	res.render('index', { title: 'Express Practice', message: 'its my frist time'});
+	
+	var mytitle = 'Express Practice';
+	var mymessage = 'its my frist time!';
+	res.render('index', { title: mytitle, message: mymessage});
 });
 
-app.get('/*', router);
+app.use('/', router);
